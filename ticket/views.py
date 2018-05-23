3eec7552f7dd1850a60b3b48a765c602d26425ab
lincc <lincc@lamudatech.com>
2018-05-23 21:08:47 +0800

    first commit

---
+++ ticket/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse(u'林次次')
